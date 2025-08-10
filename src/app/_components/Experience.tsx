0f7531a45972d24f520bebb379c057d141c007a0
Richard Joseph Omega <richardjoseph.omega@gmail.com>
2025-08-11 00:12:58 +0800

    cleanup fix

--- src/app/_components/Experience.tsx
+++ src/app/_components/Experience.tsx
@@ -3,7 +3,6 @@ import React from "react";
 
 import {
   Collapse,
-  Button,
 } from "@material-tailwind/react";
 
 import { Card } from "@/components/Card";
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,19 @@
-import { Nav } from "./_components/Nav";
 import { Header } from "./_components/Header"
 import { Skills } from "./_components/Skills"
 import { Experience } from "./_components/Experience"
 import { Education } from "./_components/Education"
 import { Projects } from "./_components/Projects"
 import { Contacts } from "./_components/Contacts"
-import { Footer } from "./_components/Footer"
 
 export default function Home() {
   return (
     <div>
-      {/* <Nav/> */}
-  
         <Header/>
         <Skills/>
         <Experience/>
         <Education/>
         <Projects/> 
-        <Contacts/>
-        {/* <Footer/> */}
-        {/* <AboutMe/> */}
-  
-      
+        <Contacts/>  
     </div>
     
   )
